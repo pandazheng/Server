ac2edd16386d7d29ccf8ba3a1d6cf1a99a0f0f6d
pandazheng <543828315@qq.com>
2013-07-14 00:02:16 -0700

    Initial Commit

---
+++ Server/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Server
+//
+//  Created by panda zheng on 13-7-14.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
